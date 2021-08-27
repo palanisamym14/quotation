c360ed63d7c299abcc006e8705fa5f673445d114
Palanisamy M <palanisamym@Palanisamys-MacBook-Air.local>
2021-08-27 08:46:13 +0530

    added the validation

--- lib/src/screens/customers/customer_detail.dart
+++ lib/src/screens/customers/customer_detail.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:quotation/src/components/add_item.dart';
-import 'package:quotation/src/components/drawer.dart';
-import 'package:quotation/src/screens/customers/customer.dart';
-import 'package:quotation/src/screens/dataGrid/grid_constant.draft.dart';
 import 'package:quotation/src/screens/quotation_history/history.dart';
 
 class CustomerDetailContainer extends StatelessWidget {
@@ -16,10 +12,56 @@ class CustomerDetailContainer extends StatelessWidget {
       appBar: AppBar(
         title: Text("Customer Detail"),
       ),
-      body: Container(
-        child: QuotationHistory(customerId:customer["id"]),
+      body: CustomScrollView(
+        slivers: [
+          // SliverPersistentHeader(
+          //   pinned: false,
+          //   delegate: MyDynamicHeader(),
+          // ),
+          SliverFillRemaining(
+              child: QuotationHistory(customerId: customer["id"]))
+        ],
+        // QuotationHistory(customerId:customer["id"]),
       ),
     );
   }
+}
 
+class MyDynamicHeader extends SliverPersistentHeaderDelegate {
+  int index = 0;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return LayoutBuilder(builder: (context, constraints) {
+      final Color color = Colors.primaries[index];
+      final double percentage =
+          (constraints.maxHeight - minExtent) / (maxExtent - minExtent);
+
+      if (++index > Colors.primaries.length - 1) index = 0;
+
+      return Container(
+        decoration: BoxDecoration(
+            boxShadow: [BoxShadow(blurRadius: 4.0, color: Colors.black45)],
+            gradient: LinearGradient(colors: [Colors.blue, color])),
+        height: constraints.maxHeight,
+        child: SafeArea(
+            child: Center(
+          child: CircularProgressIndicator(
+            value: percentage,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          ),
+        )),
+      );
+    });
+  }
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate _) => true;
+
+  @override
+  double get maxExtent => 250.0;
+
+  @override
+  double get minExtent => 80.0;
 }
